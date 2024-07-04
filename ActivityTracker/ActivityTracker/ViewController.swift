a0b4767db575a79af97374db80c2dd77cc037cbd
LTTS Rahul Salvikar <salvikar.rahul@gmail.com>
2024-07-04 11:56:32 +0530

    Initial Commit

---
+++ ActivityTracker/ActivityTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityTracker
+//
+//  Created by Rahul on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
